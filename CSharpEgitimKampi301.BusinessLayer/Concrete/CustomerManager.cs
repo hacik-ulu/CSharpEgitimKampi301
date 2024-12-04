63755bbab0dbee1dc8d5bf16b376a5dc4e4e1231
Haçik Ulu <hacikd2@gmail.com>
2024-12-04 16:31:29 +0300

    C# Eğitim Kampı Ders 19 - Business Katmanı ve Logic Kurallar.

---
+++ CSharpEgitimKampi301.BusinessLayer/Concrete/CustomerManager.cs
@@ -0,0 +1,69 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        private readonly ICustomerDal _customerDal;
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public void TAdd(Customer entity)
+        {
+            if (entity.CustomerName != "" && entity.CustomerName.Length >= 3 && entity.CustomerCity != null &&
+                entity.CustomerSurname != "" && entity.CustomerName.Length <= 30)
+            {
+                _customerDal.Add(entity);
+            }
+            else
+            {
+                throw new Exception("Ekleme işlemi gerçekleştirilemedi !");
+            }
+        }
+
+        public void TDelete(Customer entity)
+        {
+            _customerDal.Delete(entity);
+        }
+
+        public List<Customer> TGetAll()
+        {
+            //if (yetki varsa)
+            //{
+            //listeleme yap
+            //}
+            // else
+            // {
+            // uyarı ver
+            //}
+
+            return _customerDal.GetAll();
+        }
+
+        public Customer TGetById(int id)
+        {
+            return _customerDal.GetById(id);
+        }
+
+        public void TUpdate(Customer entity)
+        {
+            if (entity.CustomerId != 0 && entity.CustomerCity.Length >= 3)
+            {
+                _customerDal.Update(entity);
+            }
+            else
+            {
+                throw new Exception("Ekleme işlemi gerçekleştirilemedi !");
+            }
+        }
+    }
+}
